e8c336b8ef3c6b6e6cc155a64b48f3e10cf8f32e
Foklan <gm.foklan@gmail.com>
2023-12-13 21:21:13 +0000

    using jinja template system

---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+    1 : {
+        "title": "Hello WOrld",
+        "content": "some toneten hej pic"
+    },
+    2 : {
+        "title": "Hello world",
+        "content": "Lorem debilum"
+    },
+    3: {
+        "title": "Hello world",
+        "content": "Lorem dementum"
+    },
+    4 : {
+        "title": "Hello world",
+        "content": "Lorem kktum"
+    }
+}
